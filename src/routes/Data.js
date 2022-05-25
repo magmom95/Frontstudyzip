529ea4d6741a388549b8311844d8c536d5da3e62
magmom95 <magmom95@naver.com>
2022-05-25 14:16:43 +0900

    React Component ing

---
+++ src/routes/Data.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    year: 2001,
+    title: "그의 나이 7세",
+    LatLngX: 37.5843,
+    LatLngY: 126.9224,
+    number: 7,
+  },
+
+  {
+    year: 2001,
+    title: "그의 나이 15세",
+    LatLngX: 37.581387192133256,
+    LatLngY: 126.92505042549021,
+    number: 15,
+  },
+
+  {
+    year: 2001,
+    title: "그의 나이 26세",
+    LatLngX: 37.53070155328111,
+    LatLngY: 126.92863384189171,
+    number: 26,
+  },
+
+  {
+    year: 2001,
+    title: "그의 나이 28세",
+    LatLngX: 37.48275936584787,
+    LatLngY: 126.89827101185179,
+    number: 28,
+  },
+];
---
+++ src/routes/Young.js
@@ -0,0 +1,43 @@
+/*global kakao*/
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+// import img from "../img/7.jpg";
+import "./App.css";
+
+function Young() {
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate("/");
+  }
+  useEffect(() => {
+    var container = document.getElementById("map");
+    var options = {
+      // center: new kakao.maps.LatLng(?, ?),
+      level: 3,
+    };
+
+    var map = new kakao.maps.Map(container, options);
+    // var markerPosition  = new kakao.maps.LatLng(?, ?);
+    var marker = new kakao.maps.Marker({
+      // position: markerPosition
+    });
+    marker.setMap(map);
+  }, []);
+
+  return (
+    <div>
+      {/* <h1 className="Head">그의 나이 7세</h1> */}
+      {/* <img src={img} className="Img" alt="img" /> */}
+      <div
+        className="Map"
+        id="map"
+        style={{ width: "300px", height: "280px" }}
+      ></div>
+      <button className="Back" onClick={handleClick}>
+        이전화면으로
+      </button>
+    </div>
+  );
+}
+
+export default Young;
