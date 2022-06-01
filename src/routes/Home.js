d3ec8155195b0d51324a633b12eedbbc11b8c556
magmom95 <magmom95@naver.com>
2022-06-01 13:44:45 +0900

    mapping

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import "./App.css";
 import React, { useState } from "react";
-
 import logo from "../img/logo.gif";
 
 function Home() {
   const [selects, setSelects] = useState();
   let navigate = useNavigate();
+  const home = ["어", "디", "든", "문"];
+  const homeList = home.map((homes, index) => (
+    <button
+      key={index}
+      onClick={() => {
+        navigate(`/${index}`);
+      }}
+    >
+      {homes}
+    </button>
+  ));
+
   return (
     <div>
       <img src={logo} className="App-logo" alt="logo" />
       <select value={selects} onChange={(e) => setSelects(e.target.value)}>
+        <option value="">골라주세요</option>
         <option value="2001">7살</option>
         <option value="2009">15살</option>
         <option value="2020">26살</option>
@@ -18,14 +30,17 @@ function Home() {
       </select>
       <hr />
       <p>{selects}</p>
-      <button
+
+      <>{homeList}</>
+
+      {/* <button
         onClick={() => {
           navigate("/0");
         }}
       >
         어
-      </button>
-      <button
+      </button> */}
+      {/* <button
         onClick={() => {
           navigate("/1");
         }}
@@ -45,7 +60,7 @@ function Home() {
         }}
       >
         문
-      </button>
+      </button> */}
     </div>
   );
 }
