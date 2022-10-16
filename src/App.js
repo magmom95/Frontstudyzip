27e6e43ee31a0851aefc8f2ab04533d47c99fdae
magmom95 <64140544+magmom95@users.noreply.github.com>
2022-10-17 01:44:37 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Young from "./routes/Young";
+// import Young from "./routes/Young";
 import Home from "./routes/Home";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
@@ -8,7 +8,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Young />} />
+        {/* <Route path="/:id" element={<Young />} /> */}
         {/* <Route path="/2009" element={<Young2 />} />
         <Route path="/2019" element={<Young4 />} />
         <Route path="/2022" element={<Young5 />} /> */}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,57 +1,35 @@
-import { useNavigate } from "react-router-dom";
-import "./App.css";
-import React from "react";
-import logo from "../img/logo.gif";
-import data from "./data";
+import React, { useEffect, useState } from "react";
 
 function Home() {
-  let navigate = useNavigate();
-  // const homes = ['어', '디', '든', '문'];
-  // const homeList = homes.map((home, index) => (
-  //   <button
-  //     key={home} // key에 index를 넣는 방식은 지양함 => 항목의 순서가 바뀔 수 있는 경우 key에 인덱스를 사용하는 것은 권장하지 않습니다. 이로 인해 성능이 저하되거나 컴포넌트의 state와 관련된 문제가 발생할 수 있습니다.(https://ko.reactjs.org/docs/lists-and-keys.html)
-  //     onClick={() => {
-  //       navigate(`/${index}`);
-  //     }}
-  //   >
-  //     {home}
-  //   </button>
-  // ));
+  const [list, setList] = useState([
+    { name: "스터디" },
+    { name: "리액트" },
+    { name: "힘들다" },
+  ]);
 
-  const homeList = Object.keys(data).map((key) => {
-    return (
-      <button
-        key={key}
-        onClick={() => {
-          navigate(`/${key}`);
-        }}
-      >
-        {data[key].label}
-      </button>
-    );
-  });
+  const addItem = () => {
+    setList([{ name: "이성규" }, ...list]);
+  };
+
+  const delItem = () => {
+    setList(list.filter((l) => l.name != "스터디"));
+  };
 
   return (
-    <div>
-      <img src={logo} className="App-logo" alt="logo" />
-      <select onChange={(e) => navigate(e.target.value)}>
-        <option value="">골라주세요</option>
-        {Object.keys(data).map((key, idx) => {
-          return <option key={key} value={key}>{`${data[key].age}살`}</option>;
-        })}
-      </select>
-      <hr />
-      {/* <p>{selects}</p> */}
-      {homeList}
-      <button
-        onClick={() => {
-          navigate(`/5000`);
-        }}
-      >
-        잘못된 버튼
-      </button>
-    </div>
+    <>
+      {/* 추가 버튼과 삭제 버튼*/}
+      <input type="button" value="추가" onClick={addItem} />
+      <input type="button" value="삭제" onClick={delItem} />
+
+      <h2> 삭제는 어떻게 보일까 </h2>
+      {list.map((v, index) => (
+        /*  div 태그의 key로 배열의 index 사용*/
+        <div key={index}>
+          {" "}
+          {v.name}, idx: {index} <input type="text" />{" "}
+        </div>
+      ))}
+    </>
   );
 }
-
 export default Home;
